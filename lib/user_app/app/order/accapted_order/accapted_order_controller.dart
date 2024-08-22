f02abb0d368adb1c3e773bf9cad2478c84158779
Hafiz Bismil <hafizbismil@gmail.com>
2024-08-22 14:28:18 +0500

    Hafiz Bismil

--- lib/user_app/app/order/accapted_order/accapted_order_controller.dart
+++ lib/user_app/app/order/accapted_order/accapted_order_controller.dart
@@ -2,6 +2,12 @@ import 'package:get/get.dart';
 
 class AccaptedOrderController extends GetxController {
   static AccaptedOrderController instance = Get.find();
+  bool isSelected = false;
+  void toggleSelection() {
+    isSelected = !isSelected;
+    update();
+  }
+
   double ratings = 0.0;
   void updateRating(double rating) {
     ratings = rating;
