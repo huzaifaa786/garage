92e9fcf62f0a09d6bcad5e935dd884c6f67800a6
Hafiz Bismil <hafizbismil@gmail.com>
2024-08-22 14:31:16 +0500

    hhvhj

--- lib/user_app/app/order/accapted_order/accapted_order_view.dart
+++ lib/user_app/app/order/accapted_order/accapted_order_view.dart
@@ -5,6 +5,7 @@ import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:mobilegarage/user_app/app/order/accapted_order/accapted_order_controller.dart';
 import 'package:mobilegarage/user_app/app/order/accapted_order/component/order_card.dart';
+import 'package:mobilegarage/user_app/components/buttons/main_button.dart';
 import 'package:mobilegarage/user_app/utils/app_text/app_text.dart';
 import 'package:mobilegarage/user_app/utils/colors/app_color.dart';
 import 'package:mobilegarage/vendor_app/layout/app_layout.dart';
@@ -28,34 +29,36 @@ class _AccaptedOrderViewState extends State<AccaptedOrderView> {
         child: Padding(
           padding: const EdgeInsets.all(18.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, 
-            mainAxisAlignment: MainAxisAlignment.start,// Align items to the start
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment:
+                MainAxisAlignment.start, // Align items to the start
             children: [
               // Add the title row with "Select garage" and "(12) garages"
               Row(
-                mainAxisAlignment: MainAxisAlignment.start, 
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                
                   AppText(
                     title: 'Select garage',
-                    size: 14, 
+                    size: 14,
                     fontWeight: FontWeight.w500,
                   ),
-                 
-                  
                 ],
               ),
               Gap(7),
               Row(
-                 mainAxisAlignment: MainAxisAlignment.start,
-                children: [AppText(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  AppText(
                     title: '(12) garages',
                     size: 12,
-                    fontWeight: FontWeight.w500, 
-                    color: AppColors.primary, 
-                  ),],),
-              SizedBox(height: 10), 
-          
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.primary,
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              MainButton(title: 'next',),
+
               OrderCard(),
             ],
           ),
