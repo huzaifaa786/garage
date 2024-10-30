aac20f3d53ac77c1ba4f39368cda8e22e857b1de
usamasohi <tritechuzaifa@gmail.com>
2024-10-30 14:40:11 +0500

    pull push

--- lib/models/notifications_model.dart
+++ lib/models/notifications_model.dart
@@ -28,9 +28,9 @@ class NotificationsModel {
         seen: json['seen'] ?? false,
         body: json['body'],
         createdAt: DateTime.parse(json['created_at']),
-        userImage: json['sender']['image']??'',
-        username: json['sender']['name']??'',
-        categoryName: json['category']['name']?? '',
+        userImage: json['sender']!=null? json['sender']['image']:'',
+        username:json['sender']!=null? json['sender']['name']:'',
+        categoryName: json['category']!=null?json['category']['name']: '',
         order:
             json['order'] != null ? OrdersModel.fromJson(json['order']) : null);
   }
